d6337b690ea0b61e2fee7d829cd909f6a548eda2
Rayed <rayed.benbrahim@gmail.com>
2021-03-05 18:09:28 +0100

    feat(geoLocationService):query ratp api and build coordinates object

--- geoLocationService.ts
+++ geoLocationService.ts
@@ -1,6 +1,17 @@
-
+import axios from 'axios'
 
 export const findCoordinates = async (query) => {
-    return query
+    const request = await axios.get(`https://data.ratp.fr/api/records/1.0/search/?dataset=positions-geographiques-des-stations-du-reseau-ratp&q=${query}&facet=stop_desc`)
+    const ratpResponse = request.data
+    const coordinates = ratpResponse.records.map(result => {
+        return {
+            gps: result.fields.stop_coordinates,
+            name: result.fields.stop_name,
+            description: result.fields.stop_desc,
+            id: result.fields.stop_id
+        }
+    })
+
+    return coordinates
 }
 
