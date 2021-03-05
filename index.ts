0a1b29f257525176778ed4e7b558eb3f290b1b78
Rayed <rayed.benbrahim@gmail.com>
2021-03-05 18:30:00 +0100

    wip(swagger): add swagger to query

--- index.ts
+++ index.ts
@@ -1,13 +1,41 @@
 import express from "express"
 import {getCoordinates} from './geoLocationController'
+import swaggerJsdoc from 'swagger-jsdoc'
+import swaggerUi from 'swagger-ui-express'
 
 const app = express();
 const PORT = 8000;
 
-app.get('/', getCoordinates)
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Express API with Swagger",
+      version: "0.1.0",
+      description:
+        "This is a simple API application made with Express and documented with Swagger",
+    },
+    servers: [
+      {
+        url: "http://localhost:8000/",
+      },
+    ],
+  },
+  apis: ["/"],
+};
+
+const specs = swaggerJsdoc(options);
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(specs)
+);
+
+
   
 // TODO: GET REQUEST WITH QUERY PARAMS
-app.get('')
+app.get('/', getCoordinates)
+
 // TODO: POST REQUEST WITH BODY
 
 app.listen(PORT, () => {
